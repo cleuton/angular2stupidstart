381a4c2f482ec2733b301a007be0301f1bec3d06
Cleuton Sampaio <cleuton.melo@serpro.gov.br>
2017-06-13 09:02:54 -0300

    Transforming into Angular 4 app

--- showMyIp/app/app.component.js
+++ showMyIp/app/app.component.js
@@ -8,11 +8,11 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
-var http_1 = require('@angular/http');
-var Rx_1 = require('rxjs/Rx');
-require('rxjs/add/operator/map');
-require('rxjs/add/operator/catch');
+var core_1 = require("@angular/core");
+var http_1 = require("@angular/http");
+var Rx_1 = require("rxjs/Rx");
+require("rxjs/add/operator/map");
+require("rxjs/add/operator/catch");
 var Resultado = (function () {
     function Resultado() {
     }
@@ -38,14 +38,14 @@ var AppComponent = (function () {
             .map(function (res) { return res.json(); })
             .catch(function (error) { return Rx_1.Observable.throw(error.json().error || 'Server error'); });
     };
-    AppComponent = __decorate([
-        core_1.Component({
-            selector: 'my-app',
-            template: "\n      <h1>Welcome to: {{name}}</h1>\n      <input type=\"button\" value=\"Pesquisar\" (click)=\"pesquisar()\"/>\n      <hr/>\n      <div *ngIf=\"resultado\">\n          <table border=\"1\">\n              <thead>\n                  <tr role=\"row\">\n                      <th>Propriedade</th>\n                      <th>Valor</th>\n                  </tr>\n              </thead>\n              <tbody>\n                  <tr>\n                      <td>ip</td>\n                      <td>{{resultado.ip}}</td>\n                  </tr>\n                  <tr>\n                      <td>hostname</td>\n                      <td>{{resultado.city}}</td>\n                  </tr>\n                  <tr>\n                      <td>region</td>\n                      <td>{{resultado.region}}</td>\n                  </tr>\n                  <tr>\n                      <td>country</td>\n                      <td>{{resultado.country}}</td>\n                  </tr>\n                  <tr>\n                      <td>loc</td>\n                      <td>{{resultado.loc}}</td>\n                  </tr>\n                  <tr>\n                      <td>org</td>\n                      <td>{{resultado.org}}</td>\n                  </tr>\n              </tbody>\n          </table>        \n      </div> \n  ",
-        }), 
-        __metadata('design:paramtypes', [http_1.Http])
-    ], AppComponent);
     return AppComponent;
 }());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: 'my-app',
+        template: "\n      <h1>Welcome to: {{name}}</h1>\n      <input type=\"button\" value=\"Pesquisar\" (click)=\"pesquisar()\"/>\n      <hr/>\n      <div *ngIf=\"resultado\">\n          <table border=\"1\">\n              <thead>\n                  <tr role=\"row\">\n                      <th>Propriedade</th>\n                      <th>Valor</th>\n                  </tr>\n              </thead>\n              <tbody>\n                  <tr>\n                      <td>ip</td>\n                      <td>{{resultado.ip}}</td>\n                  </tr>\n                  <tr>\n                      <td>hostname</td>\n                      <td>{{resultado.city}}</td>\n                  </tr>\n                  <tr>\n                      <td>region</td>\n                      <td>{{resultado.region}}</td>\n                  </tr>\n                  <tr>\n                      <td>country</td>\n                      <td>{{resultado.country}}</td>\n                  </tr>\n                  <tr>\n                      <td>loc</td>\n                      <td>{{resultado.loc}}</td>\n                  </tr>\n                  <tr>\n                      <td>org</td>\n                      <td>{{resultado.org}}</td>\n                  </tr>\n              </tbody>\n          </table>        \n      </div> \n  ",
+    }),
+    __metadata("design:paramtypes", [http_1.Http])
+], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
